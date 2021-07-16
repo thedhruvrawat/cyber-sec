692fba5e0eb23c76aebb4fb7ad530f1e3250f1b6
thedhruvrawat <dhruv.noida175@gmail.com>
2021-07-16 17:23:28 +0530

    Add b64, xor and affine

---
+++ cryptography/base64decode.py
@@ -0,0 +1,5 @@
+import base64 as b64
+decoded_data = b64.b64decode("RW5jb2RlIHRoaXMgdGV4dA==")
+
+print("Decoded text is ")
+print(decoded_data)
---
+++ cryptography/base64encode.py
@@ -0,0 +1,5 @@
+import base64 as b64
+encoded = b64.b64encode("This is encoded text")
+
+print("Encoded text is: ")
+print(encoded)
